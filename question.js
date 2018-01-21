67d084484c1350db348bc3c651e4600050700ff7
Vino-git <vinoth.dj@gmail.com>
2018-01-21 22:34:53 +0400

    Matthew files

---
+++ question.js
@@ -0,0 +1,174 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    QUESTIONS_EN_US: [
+        {
+            'Who is generally accepted to be the author of the gospel of Matthew? ': [
+                'Matthew',
+                'Peter',
+                'Paul',
+                'Joseph',
+                'James',
+            ],
+        },
+        {
+            'Who told Joseph not to fear taking Mary as his wife? ': [
+                'Angel of the Lord',
+                'The Shepherds',
+                'Mary',
+                'The Wise Men',
+                'Canada',
+                'Norway',
+                'France',
+            ],
+        },
+        {
+            'Which ruler wanted the baby Jesus killed? ': [
+                'Herod',
+                'Tiberius',
+                'Augustus',
+                'Philip',
+            ],
+        },
+        {
+            'Who baptised Jesus? ': [
+                'John the Baptist',
+                'Peter',
+                'James',
+                'Matthew',
+            ],
+        },
+        {
+            'What does Jesus say that the meek shall inherit? ': [
+                'The earth',
+                'Everlasting life',
+                'The heaver and earth',
+                'The Throne of God',
+            ],
+        },
+        {
+            'What does Jesus say to do if your right hand causes you to sin? ': [
+                'Cut it off',
+                'Burn it in the fire',
+                'Tie it behind your back',
+                'Pray for forgiveness',
+            ],
+        },
+        {
+            'What was Jesus talking about when he said you can not make one hair on your head black or white? ': [
+                'Oaths',
+                'Fasting',
+                'Forgiveness',
+                'Resurrection',
+            ],
+        },
+        {
+            'How should we practise charitable works, prayers and fasting? ': [
+                'In secret',
+                'Quietly',
+                'Openly',
+                'With thanksgiving',
+            ],
+        },
+        {
+            'Where was Matthew sitting when Jesus called him? ': [
+                'On a donkey',
+                'On a donkey',
+                'By a well',
+                'Kmart',
+            ],
+        },
+        {
+            'Which prophet did Jesus refer to when some of the Scribes and Pharisees asked for a sign? ': [
+                'Jonah',
+                'Elijah',
+                'Moses',
+                'Elisha',
+            ],
+        },
+        {
+            'What was the fate of the tares in the parable of the tares? ': [
+                'Burnt',
+                'Thrown into the river',
+                'Fed to the swine',
+                'Left to rot',
+            ],
+        },
+        {
+            'How many measures of flour were used to hide the leaven in the parable of the leaven? ': [
+                'Three',
+                'Two',
+                'One',
+                'Four',
+            ],
+        },
+        {
+            'Which disciple was told to catch a fish that would hold a coin in its mouth? ': [
+                'Peter',
+                'Philip',
+                'Andrew',
+                'James',
+            ],
+        },
+        {
+            'In the parable of the workers in the vineyard, why were some of the labourers unhappy with being paid only one penny? ': [
+                'Some had worked less hours for the same pay',
+                'They were made to work overtime for no extra pay',
+                'They did not think it was enough for the type of work',
+                'They had been promised two pennies',
+            ],
+        },
+        {
+            'What objects did the ten virgins take with them in the parable? ': [
+                'Lamps',
+                'Gifts',
+                'Scrolls',
+                'Coins',
+            ],
+        },
+        {
+            'At the judgement, Jesus will separate the people like a shepherd separates the sheep and which other animals? ': [
+                'Goats',
+                'Foxes',
+                'Deer',
+                'Cows',
+            ],
+        },
+        {
+            'What did Judas do with the 30 pieces of silver that he was given to betray Jesus? ': [
+                'Gave them back',
+                'Lost them to robbers',
+                'Donated them to the treasury',
+                'Bought a field',
+            ],
+        },
+        {
+            'Other than putting a stone in front of Jesus tomb, what measure was taken to guard the tomb? ': [
+                'Set a guard to watch',
+                'Set traps',
+                'None',
+                'Told no-one of the location',
+            ],
+        },
+        {
+            'What story were the soldiers told to tell to explain how Jesus disappeared from the tomb? ': [
+                'The disciples stole him',
+                'The Sadducees stole him',
+                'He came back to life and walked away',
+                'He did not actually die and escaped',
+            ],
+        },
+        {
+            'How many woes did Jesus pronounce on the Scribes and Pharisees in Matthew\'s gospel? ': [
+                'Seven',
+                'Ten',
+                'Five',
+                'Nine',
+            ],
+        },
+    ],
+};
