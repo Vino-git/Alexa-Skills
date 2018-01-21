7a365b31ce7eb59379ec5aae028ee429b07fc0be
Vino-git <vinoth.dj@gmail.com>
2018-01-21 22:40:33 +0400

    Luke Files

---
+++ question.js
@@ -0,0 +1,171 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    QUESTIONS_EN_US: [
+        {
+            'What was the name of the angel who visited Zacharias? ': [
+                'Gabriel',
+                'Cassiel',
+                'Michael',
+                'Seraphiel',
+            ],
+        },
+        {
+            'Who had been told by the Holy Spirit that he would not see death before he had seen the Lord's Christ? ': [
+                'Simeon',
+                'Peter',
+                'Herod',
+                'John the Baptist',
+                'Judas',
+            ],
+        },
+        {
+            ' Who did Christ raise in the town of Nain? ': [
+                'Widow's son',
+                'Peter's mother-in-law',
+                'Jairus' daughter',
+                'Lazarus',
+            ],
+        },
+        {
+            ' With which parable did Jesus respond when asked by the lawyer "who is my neighbour"? ': [
+                'Good Samaritan',
+                'Ten Virgins',
+                'Sower',
+                'Workers in the Vineyard',
+            ],
+        },
+        {
+            'How many loaves did Christ mention when he spoke about a hungry friend arriving at midnight? ': [
+                'Three',
+                'Two',
+                'One',
+                'Four',
+            ],
+        },
+        {
+            'When Jesus appeared to his disciples after his resurrection what did they give him to eat?  ': [
+                'Fish',
+                'Cheese',
+                'Lamb',
+                'Bread',
+            ],
+        },
+        {
+            'Eat, drink and be merry" is a reference from which parable?   ': [
+                'Rich fool',
+                'Prodigal Son',
+                'Great Banquet',
+                'Wedding feast',
+            ],
+        },
+        {
+            ' Who was Jesus referring to when he said "go and tell that fox"?   ': [
+                'Herod',
+                'Augustus',
+                'Annas',
+                'Pilate',
+            ],
+        },
+        {
+            'What does Jesus say is of no use if it has lost its taste?  ': [
+                'Salt',
+                'Leaven',
+                'Wormwood',
+                'Wine',
+            ],
+        },
+        {
+            ' In the parable of the lost sheep how does the shepherd bring the found sheep home?   ': [
+                'On his shoulder',
+                'On a rope',
+                'Follows it home',
+                'In a cart',
+            ],
+        },
+        {
+            'How many silver coins did the woman have in the parable of the lost coin?   ': [
+                'Ten',
+                'Five',
+                'Twelve',
+                'One Hundred',
+            ],
+        },
+        {
+            'What did the Prodigal son ask for in advance?  ': [
+                'His inheritance',
+                'His dowry',
+                'His wages',
+                'His supper',
+            ],
+        },
+        {
+            'In the parable why was the unjust steward dismissed from his job?   ': [
+                'Wasting goods',
+                'Absent from work',
+                'Attacked a servant',
+                'Camel',
+            ],
+        },
+        {
+            ' In the parable of the rich man and Lazarus, the poor man was carried to whose side?  ': [
+                'Abraham/s',
+                'Issac/s',
+                'Jacob/s',
+                'David/s',
+            ],
+        },
+        {
+			' When Jesus met the ten lepers who did he tell them to go and show themselves to?   ': [
+                'Priests',
+                'Pharisees',
+                'No-one',
+                'Families',
+            ],
+        },
+        {
+            ' In the parable of the Pharisee and the tax collector how often did the Pharisee fast?  ': [
+                'Twice a week',
+                'Every day',
+                'Once a month',
+                'Once a week',
+            ],
+        },
+        {
+            ' In which city did Jesus meet Zacchaeus?  ': [
+                'Jericho',
+                'Galilee',
+                'Jerusalem',
+                'Samaria',
+            ],
+        },
+        {
+            'What did Jesus tell his disciples to sell their cloaks for if they didn't already have one?  ': [
+                'Sword',
+                'Staff',
+                'Knapsack',
+                'Map',
+            ],
+        },
+        {
+            'Who said "Jesus remember me when you come into your kingdom"?  ': [
+                'The thief on the cross',
+                'Lazarus',
+                'Nicodemus',
+                'Mary',
+            ],
+        },
+        {
+            ' How many men walked with the resurrected Jesus on the road to Emmaus?   ': [
+                'Two',
+                'Five',
+                'Seven',
+                'Eleven',
+            ],
+        },
+    ],
+};
