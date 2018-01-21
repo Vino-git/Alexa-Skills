1da7406c97f9afdcafd00e0bf09e4741dbd69229
Vino-git <vinoth.dj@gmail.com>
2018-01-21 22:46:51 +0400

    John files

---
+++ JOhn_question.js
@@ -0,0 +1,171 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    QUESTIONS_EN_US: [
+        {
+            'Why was John the Baptist sent from God?  ': [
+                'To bear witness of the light',
+                'To show humility by his simplistic life',
+                'To perform miracles and signs',
+                'To explain the writings of the prophets',
+            ],
+        },
+        {
+            'What did John the Baptist say when he saw Jesus?  ': [
+                'Behold the Lamb of God!',
+                'Behold the Saviour of the world!',
+                'Hosanna in the highest!',
+                'You are the true Son of God',
+                'Judas',
+            ],
+        },
+        {
+            '  Where did Jesus see Nathanael? ': [
+                'Under the fig tree',
+                'Under the almond branch',
+                'Within the house',
+                'At the gate of the temple',
+            ],
+        },
+        {
+            '  What happened at the marriage of Cana?  ': [
+                'Jesus turned water into wine',
+                'Jesus condemned materialism',
+                'Jesus healed a crippled man',
+                'Jesus forgave the couple\'s sins',
+            ],
+        },
+        {
+            'To whom did Jesus say, "Ye must be born again"?': [
+                'Nicodemus',
+                'Simon of Cyrene',
+                'Joseph of Arimathea',
+                'Gamaliel',
+            ],
+        },
+        {
+            ' What did Jesus say he could give the woman of Samaria?   ': [
+                'Living water',
+                'Bread of life',
+                'Forgiveness of sins',
+                'Eternal life',
+            ],
+        },
+        {
+            'How long had the infirm man lain at the pool of Bethesda?    ': [
+                '38 years',
+                '1 year',
+                '12 years',
+                '25 years',
+            ],
+        },
+        {
+            ' Which of these is not declared as a witness of Jesus?   ': [
+                'Nature and creation',
+                'John the Baptist',
+                'God',
+                'The work God gave Jesus',
+            ],
+        },
+        {
+            'What teaching of Jesus did the Jews not understand?   ': [
+			    'I am the bread which came down from heaven',
+                'The kingdom of God is nigh at hand',
+                'He who is not for me, is against me',
+                'You hypocrites, let each look first to his own house',
+            ],
+        },
+        {
+            ' Which two signs, or miracles, are also recorded in other gospel records?    ': [
+                'Feeding of the 5,000, Jesus walks on water',
+                'Feeding of the 5,000,raising of Lazarus' ',
+                'Raising of Lazarus, the draught of fishes',
+                'The ruler\'s son healed,the draught of fishes',
+            ],
+        },
+        {
+            ' How did Jesus heal the blind man?   ': [
+                'He anointed his eyes with clay and told him to wash',
+                'He touched his eyes and scales fell off them',
+                'He anointed his eyes with oil',
+                'He said the word and he was healed',
+            ],
+        },
+        {
+            ' Which profession does Jesus compare himself to spiritually?  ': [
+                'Shepherd',
+                'Fisherman',
+                'Carpenter',
+                'Tentmaker',
+            ],
+        },
+        {
+            'What did Jesus say to those who accused the adulteress?   ': [
+                'He that is without sin, let him first cast a stone',
+                'You hypocrites, let each look first to his own house',
+                'What has she to do with me?',
+                'Take her and stone her',
+            ],
+        },
+        {
+            '  What are the names of Lazarus\'s sisters?   ': [
+                'Mary and Martha',
+                'Martha and Ruth',
+                'Joanna and Salome',
+                'Salome and Mary',
+            ],
+        },
+        {
+			' What did Jesus do to the disciples before the Last Supper?   ': [
+                'Washed their feet',
+                'Exhorted them to believe',
+                'Anointed their heads',
+                'Gave them the Holy Spirit',
+            ],
+        },
+        {
+            '  What did Jesus say he would leave with the disciples? ?  ': [
+                'Peace',
+                'Altruism',
+                'Humility',
+                'Love',
+            ],
+        },
+        {
+            ' What relation was Annas to Caiaphas?   ': [
+                'Father-in-law',
+                'Son',
+                'Grandfather',
+                'Nephew',
+            ],
+        },
+        {
+            'Where was Peter standing when he denied Jesus for the second time?  ': [
+                'Warming himself at the fire',
+                'In the streets outside the high priest\'s yard',
+                'In the garden of Gethsemane',
+                'At the foot of the cross',
+            ],
+        },
+        {
+            'What colour robe did Pilate\'s soldiers put on Jesus?   ': [
+                'Purple',
+                'Blue',
+                'Brown',
+                'White',
+            ],
+        },
+        {
+            '  How many fish were caught in the miracle of the draught of fishes?    ': [
+                '153',
+                '40',
+                '120',
+                '144',
+            ],
+        },
+    ],
+};
