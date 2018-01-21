f15bbb3263b4648078306fa79cee950e0fa1c2fd
Vino-git <vinoth.dj@gmail.com>
2018-01-21 22:37:07 +0400

    Mark files

---
+++ question.js
@@ -0,0 +1,171 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    QUESTIONS_EN_US: [
+        {
+            'Who is the first prophet quoted in the Gospel of Mark?  ': [
+                'Isaiah',
+                'Jeremiah',
+                'Paul',
+                'Daniel',
+            ],
+        },
+        {
+            'In whose house did Jesus dine with publicans and sinners? ': [
+                'Levi',
+                'John',
+                'Mary',
+                'Peter',
+                'Judas',
+            ],
+        },
+        {
+            'On what day did the disciples pluck ears of corn, much to the anger of the Pharisees?  ': [
+                'Sabbath',
+                'Day of Pentecost',
+                'First day of the week',
+                'Day of Atonement',
+            ],
+        },
+        {
+            'What sin does Jesus say is unforgivable? ': [
+                'Blasphemy against the Holy Spirit',
+                'Murder',
+                'Theft',
+                'Greed',
+            ],
+        },
+        {
+            'Where was Jesus when he taught the parable of the sower?  ': [
+                'In a boat',
+                'In the temple',
+                'On the coast',
+                'On a mountain',
+            ],
+        },
+        {
+            'Which relation of Jairus did Jesus heal?  ': [
+                'Daughter',
+                'Nephew',
+                'Uncle',
+                'Mother',
+            ],
+        },
+        {
+            'What did Jesus tell the twelve to shake off their feet if people refused to hear the gospel message?  ': [
+                'Dust',
+                'Spiders',
+                'Feathers',
+                'Sacrifices',
+            ],
+        },
+        {
+            'At the feeding of the 5,000, how many fish did they have?  ': [
+                '2',
+                '4',
+                '8',
+                '7',
+            ],
+        },
+        {
+            'This people honours me with their ___ but their heart is far from me? ': [
+                'Lips',
+                'Ears',
+                'Hands',
+                'Sacrifices',
+            ],
+        },
+        {
+            'What are believers told to �take up� if they are to follow Jesus?  ': [
+                'Their cross',
+                'Their money',
+                'Their faith',
+                'Their bed',
+            ],
+        },
+        {
+            'During the transfiguration, who appeared to Jesus and the three disciples?  ': [
+                'Moses and Elijah',
+                'Adam and Noah',
+                'Abraham and David',
+                'Enoch and Solomon',
+            ],
+        },
+        {
+            'What is it easier for a camel to go through than for a rich man to enter the kingdom of God? ': [
+                'Eye of a needle',
+                'A thimble',
+                'The gates of Jerusalem',
+                'An ear of corn',
+            ],
+        },
+        {
+            'What did Jesus ride into Jerusalem on?  ': [
+                'Colt',
+                'Elephant',
+                'Donkey',
+                'Camel',
+            ],
+        },
+        {
+            ' What did the Sadducees not believe in or practice? ': [
+                'Resurrection',
+                'The Law',
+                'Sacrifices',
+                'God',
+            ],
+        },
+        {
+            'How many mites (copper coins) did the poor widow give to the treasury?  ': [
+                '2',
+                '4',
+                '3',
+                '5',
+            ],
+        },
+        {
+            'Where did Jesus say that those in Judea should flee to when they saw the abomination of desolation set up? ': [
+                'The mountains',
+                'The River Jordan',
+                'Bethlehem',
+                'The coast',
+            ],
+        },
+        {
+            'After the last supper, what did Jesus and his disciples do before going to the Mount of Olives? ': [
+                'Sung a hymn',
+                'Read the prophecy of Daniel',
+                'Changed their shoes',
+                'Offered a sacrifice',
+            ],
+        },
+        {
+            'Whose ear was cut off with a sword at the arrest of Jesus?  ': [
+                'Servant of the High Priest',
+                'Peter',
+                'Judas Iscariot',
+                'Mark',
+            ],
+        },
+        {
+            'While on the cross, who did the bystanders believe that Jesus was calling for?  ': [
+                'Elijah',
+                'Eli',
+                'Eliah',
+                'Elisha',
+            ],
+        },
+        {
+            ' Who went to the tomb of Jesus with Mary Magdalene and Mary (mother of James) to anoint Jesus?  ': [
+                'Salome',
+                'Martha',
+                'Joanna',
+                'Elizabeth',
+            ],
+        },
+    ],
+};
